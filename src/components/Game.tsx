85c0bd7d3c0fb94fc17d6c0d53131f7f27bd4156
omar <omarhrn@gmail.com>
2025-02-15 15:33:03 -0300

    Seleccionando la respuesta correcta o incorrecta con estilos

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,36 +1,65 @@
-import { Card,List,ListItem,ListItemButton,ListItemText,Typography } from "@mui/material"
+import {
+  Card,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { useQuestionsStore } from "../store/questions";
 import { type Question as QuestionType } from "../interfaces/types";
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-const Question = ({info}: {info: QuestionType}) => {
+const getBackgroundColor = (info: QuestionType, index: number) => {
+  const { correctAnswer, userSelectedAnswer } = info;
+
+  if (userSelectedAnswer == null) return "transparent";
+  if (index !== correctAnswer && index !== userSelectedAnswer) return 'trasnparent';
+  if (index === correctAnswer) return 'green';
+  if (index === userSelectedAnswer) return 'red';
+
+  return "transparent";
+};
+
+const Question = ({ info }: { info: QuestionType }) => {
+  const selectAnswer = useQuestionsStore((state) => state.selectAnswer);
+
+  const handleClick = (answerIndex: number) => () => {
+    selectAnswer(info.id, answerIndex);
+  };
+
   return (
-    <Card variant="outlined" sx={{ marginTop: 4, textAlign: "left", bgcolor: '#222', p: 2,  }}>
-      <Typography variant="h5" >
-        {info.question}
-      </Typography>
+    <Card
+      variant="outlined"
+      sx={{ marginTop: 4, textAlign: "left", bgcolor: "#222", p: 2 }}
+    >
+      <Typography variant="h5">{info.question}</Typography>
 
       <SyntaxHighlighter language="javascript" style={gradientDark}>
         {info.code}
       </SyntaxHighlighter>
 
-      <List sx={{ bgcolor: '#333'}} disablePadding>
+      <List sx={{ bgcolor: "#333" }} disablePadding>
         {info.answers.map((answer, index) => (
           <ListItem key={index} divider disablePadding>
-            <ListItemButton>
-              <ListItemText primary={answer} sx={{textAlign: 'center'}} />
+            <ListItemButton
+              disabled={info.userSelectedAnswer != null}
+              onClick={handleClick(index)}
+              sx={{ backgroundColor: getBackgroundColor(info, index) }}
+            >
+              <ListItemText primary={answer} sx={{ textAlign: "center" }} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
     </Card>
-  )
-}
+  );
+};
 
-export function Game () {
+export function Game() {
   const questions = useQuestionsStore((state) => state.questions);
-  const currentQuestion = useQuestionsStore((state) => state.currentQuestion); 
+  const currentQuestion = useQuestionsStore((state) => state.currentQuestion);
 
   const questionInfo = questions[currentQuestion];
 
@@ -38,5 +67,5 @@ export function Game () {
     <div>
       <Question info={questionInfo} />
     </div>
-  )
+  );
 }
