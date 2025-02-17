d6dd8f2f5f4545642714897c2050a923c1ce95c0
omar <omarhrn@gmail.com>
2025-02-17 13:29:11 -0300

    Finalización del proyecto

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -10,6 +10,7 @@ interface State {
   selectAnswer: (questionId: number, answerIndex: number) => void;
   goNextQuestion: () => void;
   goPreviousQuestion: () => void;
+  reset: () => void;
 }
 
 export const useQuestionsStore = create<State>()(persist((set, get) => {
@@ -59,6 +60,9 @@ export const useQuestionsStore = create<State>()(persist((set, get) => {
       if (previousQuestion >= 0) {
         set({ currentQuestion: previousQuestion })
       }
+    },
+    reset: () => {
+      set({ questions: [], currentQuestion: 0 })
     }
   }
 }, {
